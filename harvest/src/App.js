c9c181a1971a4ddd9bf1fda772a095f06be1e806
E MA <ema@live.nmhu.edu>
2019-08-22 22:01:02 -0700

    create app

---
+++ harvest/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+      <div class = "left"  >
+        </div>
+        <div class = "right" >
+          <img src=""> </img>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
