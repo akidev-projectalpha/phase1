ba8ef1b37080683688db912ed15834ed99c18b77
E MA <ema@live.nmhu.edu>
2019-08-24 00:04:05 -0700

    create app

--- harvest/src/App.js
+++ harvest/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import hands from './img/hands-holding-smartphone-bar-chart-rocket-launch_1262-20647.jpg';
+import logo from "./img/logo.png" ;
 import './App.css';
+
+console.log(logo);
 
 function App() {
   return (
     <div className="App">
-      <body>
-      <div class = "left"  >
+      <div className = "left"  >
+        <img className="logo" src={logo} alt="logo" ></img>
+        <div className = "bar" >
+        <button type="button"> Log in using harvest </button>
+        <button> Saleforce(Sandbox) </button>
+        <button> Saleforce(Production,Developer) </button>
         </div>
-        <div class = "right" >
-          <img src=""> </img>
         </div>
-      </body>
+        <div className = "right" >
+          <img src={hands} alt="mainImage" className = "mainImage"/>
+        </div>
     </div>
   );
 }
